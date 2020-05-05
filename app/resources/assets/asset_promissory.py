fddf6f21f900193148b345f30be3f967acc43947
Bar Castel <castel.bar8@gmail.com>
2020-05-05 20:43:18 +0300

    functionality works

--- app/resources/assets/asset_promissory.py
+++ app/resources/assets/asset_promissory.py
@@ -1,23 +1,29 @@
-# import json
-#
-# from bson import ObjectId
-# from flask import request, jsonify
-# from flask_restful_swagger_3 import Resource, swagger
-#
-# from app.adapters.db_adapter import update, to_json
-# from app.decorators.auth_decorators import requires_auth
-# from app.models.assetmodel import Asset
-# from app.resources.assets.asset_docs import asset_patch_promissory_doc
-#
-#
-# class AssetPromissory(Resource):
-#     # @requires_auth
-#     @swagger.doc(asset_patch_promissory_doc)
-#     def patch(self, asset_id):
-#         # Todo: upload file S3 and save its usage url to mongo
-#         # Todo: check if url contains uniq substring
-#         asset = Asset.objects.get(id=ObjectId(asset_id))
-#         promissory_file = request.files['promissory']
-#
-#         update(asset)
-#         return jsonify({"patched asset_id": str(asset_id)})
+import boto3
+from bson import ObjectId
+from flask import request, jsonify
+from flask_restful_swagger_3 import Resource, swagger
+
+from app.adapters.db_adapter import update
+from app.models.assetmodel import Asset
+from app.resources.assets.asset_docs import asset_patch_promissory_doc
+from app.settings import BUCKET_NAME
+
+
+class AssetPromissory(Resource):
+    # @requires_auth
+    @swagger.doc(asset_patch_promissory_doc)
+    def patch(self, asset_id):
+        asset = Asset.objects.get(id=ObjectId(asset_id))
+        promissory_file = request.files['promissory']
+        # modified_filename = secure_filename(promissory_file.filename)
+        aws_key = '{}/promissory_note'.format(asset_id)
+        s3 = boto3.resource('s3')
+
+        s3.Bucket(BUCKET_NAME).put_object(Key=aws_key,
+                                          Body=promissory_file,
+                                          ContentDisposition="inline",
+                                          ContentType="application/pdf",
+                                          ACL='public-read')
+        asset.promissory_note_url = 'https://{}.s3.amazonaws.com/{}'.format(BUCKET_NAME, aws_key)
+        update(asset)
+        return jsonify({"promissory note url:": asset.promissory_note_url})
--- app/settings.py
+++ app/settings.py
@@ -13,3 +13,11 @@ DATABASE_AUTH = db_section['database_auth']
 
 api_section = config['api_section']
 APP_SECRET_KEY = api_section['app_secret_key'].encode('UTF-8')
+
+
+import os
+ACCESS_KEY = 'ASIAQK5OE254EBTIPQ4D'
+SECRET_KEY = 'x/kiO9BkwkE1W7qq39/oJi8g3AVuhpuBOhAnP1tJ'
+AWS_TOKEN = 'FwoGZXIvYXdzEGYaDIIOiCobyRvlT/cPGiLGAXFKroT9XZo6SdhZrB2a5ROsR3DJNATEsVhKxkKCxaNcXX53PGhxOF5au++GR5Ndox0YNjn3J/wovRV2tKP5caiG7peV3oL53LxjHVvbzubZCpPYXIZRH3t8wWjfAL0wi3wyeH6fHg41uu98ydrMo+Vzr3iSHSFmbHL00CIH5BQl/mcKoaZxDt3auc586QWuzFuKq5iQCDd8FCtmV0+H1xUhTCZLMJiud9E+C5nuZCkEhtXPzm0nlHetl7s2SLrvST0ASUXhJyiUwMX1BTIt6wGCFMlXVaTeUE23N/GmrMSyCVmmnT1X+hTRcJfDm/Xog0ir6mbN/+gO2Bna'
+S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format('properit-s3')
+BUCKET_NAME = 'properit-s3'
