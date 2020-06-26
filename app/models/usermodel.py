275998b148b71cd5a67c86012ea4c5fdc75c42f2
Bar Castel <castel.bar8@gmail.com>
2020-06-26 15:48:15 +0300

    reroll usermodel

--- app/models/usermodel.py
+++ app/models/usermodel.py
@@ -1,4 +1,4 @@
-from mongoengine import Document, StringField, DateTimeField, EmailField, BooleanField, DictField
+from mongoengine import Document, StringField, DateTimeField, EmailField, BooleanField
 
 
 class UserModel(Document):
@@ -7,7 +7,6 @@ class UserModel(Document):
     phone = StringField(required=True)
     email = EmailField(required=True, unique=True)
     password = StringField(required=True)
-    payment_details = DictField(required=False)
     is_tenant = BooleanField(default=False)
     is_owner = BooleanField(default=False)
     creation_date = DateTimeField()
