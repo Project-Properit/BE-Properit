b2efd766f19decae93e86061362cfc9b4c18a920
Bar Castel <castel.bar8@gmail.com>
2020-07-02 16:57:23 +0300

    payment methond none

--- app/models/paymentmodel.py
+++ app/models/paymentmodel.py
@@ -5,8 +5,8 @@ class PaymentModel(Document):
     pay_from = StringField(required=True)
     pay_to = StringField(required=True)
     amount = FloatField(required=True)
-    method = StringField(required=True)
     is_open = BooleanField(default=True)
+    method = StringField(default=None)
     when_payed = DateTimeField(default=None)
     creation_date = DateTimeField()
     meta = {'collection': 'Payments'}
